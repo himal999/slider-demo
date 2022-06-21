374b9fea2645750256aa2aa8863af10b1f386674
Himal <92311264+himal999@users.noreply.github.com>
2022-06-21 14:49:59 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+export  const data = [
+    {
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7x3h3xO8ByBvMro84x1d0qh4g78-oB9Nyo_-Y6Eb2wU1axDHSGGxnpw8YH9SJegimNm0&usqp=CAU", 
+        name:"Name 1",
+        positon:"Softweare Engineer"
+    },
+    {
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJj2n8N3tLQGYPSkPKpxOONi_qabl9gVobPz6gIWLL3l3D1iohmyTY3wLN6iRrFvJmEq8&usqp=CAU",
+        name:"Name 2",
+        positon:"Team Leader"
+    },
+    {
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPFQjJyrYLNvkq1vDUBDy1vvINKDWfbd3DUo2LX3EQ_vatA5egBip3vjS1OJM-lxPCrl8&usqp=CAU",
+        name:"Name 3",
+        positon:"CEO"
+    },
+    {
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwhVgE7vmoO6zhcTaviHW0one7QW9FP73-hzv9i_XE7_mR-K-J1pWdKNbMDMhtNDWtzLs&usqp=CAU",
+        name:"Name 4",
+        positon:"Audit"
+    },
+    {
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT-Cv34azK_m1x7Z_2MPGAb6E6P88hjD9ljBydpBDgTNqWXJ1g-9T3C4apwL9fUFZmDdw&usqp=CAU",
+        name:"Name 5",
+        positon:"Tech Leader"
+    }
+];
